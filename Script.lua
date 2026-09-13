41d41fd1c680bcb8266a8fb3ee703dd4028b91e1
Xi <valkvfx84@gmail.com>
2026-09-13 11:20:49 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://protected.obscuravm.com/d9e5ec7ed05f165a591b20b4/1bf4ba0b94aa296a4ddadf26c97e67beb2d9274d702e77e8/download"))()
+loadstring(game:HttpGet("https://api.obscuravm.com/scripts/3455061399754060588"))()
